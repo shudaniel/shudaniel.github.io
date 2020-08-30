2b1cdcb70db49233d9acfcfe7cb2bc730ad760d6
Daniel Shu <danielshu98@gmail.com>
2020-08-30 00:10:33 -0700

    Updates

---
+++ precache-manifest.ec3404b4abf5123f8c191a307c776a45.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a8d73e743a7cccc7bae1df02cc9cda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b1ed88cdf368440f81d",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ddaf41c7a3d20455d5fc",
+    "url": "/static/css/main.17e3d511.chunk.css"
+  },
+  {
+    "revision": "0b1ed88cdf368440f81d",
+    "url": "/static/js/2.4fb0a982.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4fb0a982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddaf41c7a3d20455d5fc",
+    "url": "/static/js/main.610fa62b.chunk.js"
+  },
+  {
+    "revision": "7b3037869eaf6d65db9b",
+    "url": "/static/js/runtime-main.e241b5ea.js"
+  },
+  {
+    "revision": "78c2234d68e3191487ad56ab6b49b5b4",
+    "url": "/static/media/anacapa-screenshot.78c2234d.jpg"
+  },
+  {
+    "revision": "0f0fce174e6e780c09c4e8f054061723",
+    "url": "/static/media/blockchain.0f0fce17.png"
+  },
+  {
+    "revision": "1c64990c75ed6dc44db8ed8e323923c0",
+    "url": "/static/media/folder.1c64990c.png"
+  },
+  {
+    "revision": "25b12ca308bd774431378bde713d76ed",
+    "url": "/static/media/hyperloop-screenshot.25b12ca3.png"
+  },
+  {
+    "revision": "6a347940f064146525be36b805414901",
+    "url": "/static/media/kubernetes.6a347940.png"
+  },
+  {
+    "revision": "25b20365b03b3542fc0d7c738adaefeb",
+    "url": "/static/media/landscape-profile.25b20365.jpg"
+  },
+  {
+    "revision": "4d8a2c58feba4c6a98dfa0da84cdc945",
+    "url": "/static/media/personalwebsite-screenshot.4d8a2c58.png"
+  },
+  {
+    "revision": "d81a1f1ecbb8cfc5c1d1fc47ea75e513",
+    "url": "/static/media/portrait-profile.d81a1f1e.jpg"
+  },
+  {
+    "revision": "be170eb029ace787b59d6f12090f5582",
+    "url": "/static/media/reddit.be170eb0.png"
+  },
+  {
+    "revision": "f3ced0cb902a80598962a76328e42a31",
+    "url": "/static/media/spacemonitor-screenshot.f3ced0cb.png"
+  },
+  {
+    "revision": "811c9b12f2796c805ebde520702598a9",
+    "url": "/static/media/timelines-screenshot.811c9b12.png"
+  }
+]);
